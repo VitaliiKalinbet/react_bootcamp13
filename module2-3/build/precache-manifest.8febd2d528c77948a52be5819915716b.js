6315f4c4516c5898200136ffc00e426b21f7c3b3
Vitalii Kalinbet <bosssvit@MacBook-Pro-Vitalii.local>
2019-08-29 10:35:24 +0300

    add build

---
+++ module2-3/build/precache-manifest.8febd2d528c77948a52be5819915716b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "037ee42c0786ac9703a39647848ca6e7",
+    "url": "/react_bootcamp13/module2-3/build/index.html"
+  },
+  {
+    "revision": "25861baae7078b620651",
+    "url": "/react_bootcamp13/module2-3/build/static/css/main.a89f13b7.chunk.css"
+  },
+  {
+    "revision": "1599f434e48f60576f9e",
+    "url": "/react_bootcamp13/module2-3/build/static/js/2.ace9b535.chunk.js"
+  },
+  {
+    "revision": "25861baae7078b620651",
+    "url": "/react_bootcamp13/module2-3/build/static/js/main.2cf7a9ed.chunk.js"
+  },
+  {
+    "revision": "c8711a2c7cc9106b5229",
+    "url": "/react_bootcamp13/module2-3/build/static/js/runtime~main.fe3dc655.js"
+  }
+]);
