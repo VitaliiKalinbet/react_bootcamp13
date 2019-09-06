4b084e30b760f3b29cb56a6e71df24d596dc4d23
VitaliiKalinbet <bosssvit@gmail.com>
2019-09-06 13:09:44 +0300

    done module12 animation

---
+++ module12/src/components/App.js
@@ -0,0 +1,40 @@
+import React, { Component } from 'react';
+import faker from 'faker';
+import List from './List/List';
+import Button from './Button/Button';
+import Menu from './Menu/Menu';
+
+export default class App extends Component {
+  state = {
+    items: [],
+  };
+
+  addItem = () => {
+    this.setState(prevState => ({
+      items: [
+        ...prevState.items,
+        { id: faker.random.uuid(), text: faker.lorem.words(5) },
+      ],
+    }));
+  };
+
+  removeItem = id => {
+    this.setState(prevState => ({
+      items: prevState.items.filter(item => item.id !== id),
+    }));
+  };
+
+  render() {
+    const { items } = this.state;
+
+    return (
+      <>
+        <Menu />
+        <hr />
+        <Button label="Add Item" onClick={this.addItem} />
+        <hr />
+        <List items={items} onDelete={this.removeItem} />
+      </>
+    );
+  }
+}
